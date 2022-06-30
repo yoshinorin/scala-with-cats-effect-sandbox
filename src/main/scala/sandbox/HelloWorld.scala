1214ecca05f83e9615892dee179c549d3d821df5
yoshinorin <yoshinorin.net@outlook.com>
2022-07-01 08:55:44 +0900

    update

--- src/main/scala/sandbox/HelloWorld.scala
+++ src/main/scala/sandbox/HelloWorld.scala
@@ -1,8 +1,38 @@
 package sandbox
 
 import cats.effect.IO
+import cats.effect._
 
 object HelloWorld {
 
   def say(): IO[String] = IO.delay("Hello Cats!")
+
+  def print(): IO[Unit] = {
+    IO.println("Hello") flatMap { _ =>
+      IO.println("World")
+    }
+  }
+  /*
+  scala> import cats.effect.unsafe.implicits.global
+  import cats.effect.unsafe.implicits.global
+
+  scala> sandbox.HelloWorld.print().unsafeRunSync
+  Hello
+  World
+  */
+
+  def printForComprehensions(): IO[Unit] = {
+    for {
+      _ <- IO.println("Hello")
+      _ <- IO.println("World")
+    } yield ()
+  }
+  /*
+  scala> import cats.effect.unsafe.implicits.global
+  import cats.effect.unsafe.implicits.global
+
+  scala> sandbox.HelloWorld.printForComprehensions().unsafeRunSync()
+  Hello
+  World
+  */
 }
--- src/test/scala/sandbox/HelloWorldSpec.scala
+++ src/test/scala/sandbox/HelloWorldSpec.scala
@@ -1,9 +1,12 @@
 package sandbox
 
 import cats.effect.IO
+import cats.effect._
 import cats.effect.testing.specs2.CatsEffect
 import org.specs2.mutable.Specification
+import cats.effect.unsafe.implicits.global
 
+// testOnly sandbox.ExampleSpec
 class ExampleSpec extends Specification with CatsEffect {
   "examples" should {
     "say hello" in {
